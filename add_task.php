b52686117fec7afb487c15ab96d520deab83df97
dzaky1201 <dzakyhdr@gmail.com>
2021-10-16 10:54:41 +0700

    tambah fitur add

--- add_task.php
+++ add_task.php
@@ -1,11 +1,21 @@
 <?php
+ $db = new SQLite3('db/todo.db');
+
+
 
 // Cek apakah ada task baru yang diinput oleh user
 if (isset($_POST['input_task'])) {
+
     $newTask = $_POST['input_task'];
 
+
+    // echo "INSERT INTO tasks name VALUE '$newTask'";
+
+    $statement = $db->prepare("INSERT INTO tasks (name) VALUES ('$newTask')");
+    $results = $statement->execute();
+
     // @TODO: Insert task di database
-    echo "Ops.. fungsi tidak ditemukan. Buat fungsi untuk insert task di database";
+    // echo "Ops.. fungsi tidak ditemukan. Buat fungsi untuk insert task di database";
 
     //  Function untuk redirect ke halaman index.php
     //  header('Location: index.php');
@@ -16,3 +26,5 @@ if (isset($_POST['input_task'])) {
     ***/
     header('Location: index.php');
 }
+
+?>
