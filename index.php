29798a6aba3f9fefa7b3cb7922560030d2209cbe
Muhamad Rizki <gramansite@gmail.com>
2021-10-16 12:14:58 +0700

    [Final Design]

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
         <link rel="stylesheet" type="text/css" href="css/style.css"/>
         <link rel="stylesheet"
               href="https://fonts.googleapis.com/css2?family=Open+Sans">
-
+        <!-- FontAwesome 5 -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"
                 integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
                 crossorigin="anonymous"></script>
@@ -17,13 +18,17 @@
     <body>
         <div class="main-app">
             <div class="container">
+              <div class="icon">
+                <i class="fas fa-chevron-left"></i>
+                <img src="https://img2.pngdownload.id/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg" alt="">
+              </div>
               <div class="top">
                 <div class="todo-cd">
                 <h1 class="title">Todo App</h1>
                 <div class="section-input">
                     <form action="add_task.php" method="POST">
                         <input type="text" class="task-input" id="input-field" name="input_task" placeholder="Tambahkan task baru"/>
-                        <button type="submit" class="btn-add-task">Add</button>
+                        <button type="submit" class="btn-add-task"><i class="fas fa-plus"></i></button>
                     </form>
                 </div>
                 <div class="section-task">
@@ -42,7 +47,7 @@
                     <?php } ?>
                 </div>
                 <div class="section-task-done">
-                    <h4>Completed</h4>
+                    <h3>Completed</h3>
                     <?php while ($todo = $results->fetchArray()) {
                             if ($todo['is_done'] == 1) { ?>
                         <div class="task-item" style="margin: 20px 0; border: solid 1px cornflowerblue">
